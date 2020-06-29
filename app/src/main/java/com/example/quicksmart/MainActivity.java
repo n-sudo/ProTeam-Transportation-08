531e41625c87563652e87509f58cb891519b5f5d
Nate C <nathan,craane@outlook.com>
2020-06-29 13:39:26 -0500

    "added viewMap functionality"

--- app/src/main/java/com/example/quicksmart/MainActivity.java
+++ app/src/main/java/com/example/quicksmart/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.quicksmart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,25 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button viewMap = (Button) findViewById(R.id.button);
+        viewMap.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                toMap();
+            }
+        });
+
+
+    }
+
+    private void toMap()
+    {
+
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
 
     }
 
