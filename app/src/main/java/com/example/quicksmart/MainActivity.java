e19c5e6d6ba003cf2c782be455de0a69cd7650b0
Nate C <nathan,craane@outlook.com>
2020-07-01 16:31:58 -0500

    "Started integrating places with MapView (with little to no success)"

--- app/src/main/java/com/example/quicksmart/MainActivity.java
+++ app/src/main/java/com/example/quicksmart/MainActivity.java
@@ -9,6 +9,13 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.model.TypeFilter;
@@ -18,9 +25,15 @@ import com.google.android.libraries.places.widget.listener.PlaceSelectionListene
 
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        OnMapReadyCallback,
+        GoogleMap.OnMarkerDragListener,
+        GoogleMap.OnMarkerClickListener
+{
 
     private static final String TAG = "Tag"; // For place autocomplete
+    private Marker destination;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +58,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPlaceSelected(Place place) {
                 Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                moveDestination(place);
             }
             @Override
             public void onError(Status status) {
                 Log.i(TAG, "An error occurred: " + status);
             }
         });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
 
         Button viewMap = (Button) findViewById(R.id.button);
         viewMap.setOnClickListener(new View.OnClickListener()
@@ -72,5 +92,61 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        // add a marker at indianapolis
+        LatLng indy = new LatLng(39.768402, -86.158066);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom( indy,13.75f ));
+        mMap.setOnMarkerClickListener(this);
+        mMap.setOnMarkerDragListener(this);
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+
+    }
+
+    /* https://parallelcodes.com/android-google-map-add-autocomplete-place-search/ */
+    private void moveDestination(Place place){
+
+        try
+        {
+
+            if(mMap != null)
+            {
+
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 4));
+                destination = mMap.addMarker(new MarkerOptions().position(place.getLatLng())
+                        .title("Name:" + place.getName() + ". Address:" + place.getAddress()));
+
+            }
+
+        }
+        catch(Exception ex)
+        {
+
+        }
+
+    }
 
 }
