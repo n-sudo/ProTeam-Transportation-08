0318db0841a03cc3b8d716f58832cd3eeaf15feb
Nate C <nathan,craane@outlook.com>
2020-07-02 12:19:17 -0500

    "location services and places integrated with main workflow"

--- app/src/main/java/com/example/quicksmart/MainActivity.java
+++ app/src/main/java/com/example/quicksmart/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.quicksmart;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -28,36 +41,51 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity implements
         OnMapReadyCallback,
         GoogleMap.OnMarkerDragListener,
-        GoogleMap.OnMarkerClickListener
-{
+        GoogleMap.OnMarkerClickListener {
 
     private static final String TAG = "Tag"; // For place autocomplete
+
+    private LocationManager locationManager;
+    private LatLng bestLocation;
+
     private Marker destination;
+    private Marker currentLocation;
     private GoogleMap mMap;
+    private TextView destinationText;
+    private TextView locationText;
+    private PlacesClient placesClient;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Initialize Places SDK
         Places.initialize(getApplicationContext(), "AIzaSyA1_YNNgIOgBvnU78_TUVItovD_pRDAIjU");
-        // Create a new Places client instance
-        PlacesClient placesClient = Places.createClient(this);
+
+        placesClient = Places.createClient(this);
+
+        destinationText = (TextView)  findViewById(R.id.destinationText);
+        locationText = (TextView) findViewById(R.id.locationText);
 
         //Initialize the AutocompleteSupportFragment
         AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
                 getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
         autocompleteFragment.setTypeFilter(TypeFilter.ADDRESS);
         autocompleteFragment.setCountry("US");
+
         // Specify the types of place data to return
-        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG));
+
         // Set up a PlaceSelectionListener to handle the response.
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
             public void onPlaceSelected(Place place) {
                 Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                destinationText.setText( "Destination: " + place.getName() + ", (" + place.getAddress() + ")");
+                destinationText.setVisibility(View.VISIBLE);
                 moveDestination(place);
             }
             @Override
@@ -66,11 +94,12 @@ public class MainActivity extends AppCompatActivity implements
             }
         });
 
+        // setup the map fragment
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+
+        // call onMapReadyCallback when the map is ready
         mapFragment.getMapAsync(this);
-
-
 
         Button viewMap = (Button) findViewById(R.id.button);
         viewMap.setOnClickListener(new View.OnClickListener()
@@ -82,6 +111,76 @@ public class MainActivity extends AppCompatActivity implements
             }
         });
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+
+        if(requestCode == 1) {
+
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                System.out.println(" [31m Location permissions granted, starting location");
+
+            }
+
+        }
+
+    }
+
+    private final LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+
+            locationText.setText("location: " + location.getLatitude() + " , " +  location.getLongitude());
+            locationText.setVisibility(View.VISIBLE);
+
+            currentLocation = mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("current location"));
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MainActivity.this, "Best Provider update", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+
+    };
+
+    /* https://github.com/obaro/SimpleLocationApp/blob/master/app/src/main/java/com/sample/foo/simplelocationapp/MainActivity.java */
+    private boolean isLocationEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private boolean checkLocation() {
+        if(!isLocationEnabled())
+            showAlert();
+        return isLocationEnabled();
+    }
+
+    /* https://github.com/obaro/SimpleLocationApp/blob/master/app/src/main/java/com/sample/foo/simplelocationapp/MainActivity.java */
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location")
+                .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " +
+                        "use this app")
+                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    }
+                });
+        dialog.show();
     }
 
     private void toMap()
@@ -97,6 +196,26 @@ public class MainActivity extends AppCompatActivity implements
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+
+            return;
+
+        }
+
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 2 * 60 * 1000, 10, locationListener);
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
 
         // add a marker at indianapolis
         LatLng indy = new LatLng(39.768402, -86.158066);
@@ -139,6 +258,8 @@ public class MainActivity extends AppCompatActivity implements
                 destination = mMap.addMarker(new MarkerOptions().position(place.getLatLng())
                         .title("Name:" + place.getName() + ". Address:" + place.getAddress()));
 
+            }else{
+
             }
 
         }
@@ -148,5 +269,6 @@ public class MainActivity extends AppCompatActivity implements
         }
 
     }
+
 
 }
--- app/src/main/java/com/example/quicksmart/MapsActivity.java
+++ app/src/main/java/com/example/quicksmart/MapsActivity.java
@@ -1,18 +1,13 @@
 package com.example.quicksmart;
-import android.content.pm.PackageManager;
+
 import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,8 +15,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 /* NOTE: code borrows heavily from the tutorial provided in google's online docs, specifically the one found here:
  * https://stackoverflow.com/questions/41613511/my-permissions-request-access-fine-location-permission-android-not-recognised-in */
@@ -52,6 +45,8 @@ public class MapsActivity extends FragmentActivity implements
 
         debug = (TextView) findViewById(R.id.text);
 
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -65,107 +60,7 @@ public class MapsActivity extends FragmentActivity implements
         mapFragment.getMapAsync(this);
 
         /* get location permission */
-        getLocationPermission();
 
-    }
-
-    public void getLocationPermission()
-    {
-        /*
-         * Request location permission, so that we can get the location of the
-         * device. The result of the permission request is handled by a callback,
-         * onRequestPermissionsResult.
-         */
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            locationGranted = true;
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-        }
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults)
-    {
-        locationGranted = false;
-        switch (requestCode)
-        {
-            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION:
-            {
-
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
-
-                    locationGranted = true;
-                    locationProvider = LocationServices.getFusedLocationProviderClient(this);
-
-                }
-
-            }
-
-        }
-
-    }
-
-    private void updateLocationUI()
-    {
-        if (mMap == null) {
-            return;
-        }
-        try {
-            if (locationGranted) {
-                mMap.setMyLocationEnabled(true);
-                mMap.getUiSettings().setMyLocationButtonEnabled(true);
-            } else {
-                mMap.setMyLocationEnabled(false);
-                mMap.getUiSettings().setMyLocationButtonEnabled(false);
-                LKL = null;
-                getLocationPermission();
-            }
-        } catch (SecurityException e)  {
-            Log.e("Exception: %s", e.getMessage());
-        }
-
-    }
-
-    private void getDeviceLocation()
-    {
-        /*
-         * Get the best and most recent location of the device, which may be null in rare
-         * cases when a location is not available.
-         */
-        try {
-            if (locationGranted) {
-                Task<Location> locationResult = locationProvider.getLastLocation();
-                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Location> task) {
-                        if (task.isSuccessful()) {
-                            // Set the map's camera position to the current location of the device.
-                            LKL = task.getResult();
-                            if (LKL != null) {
-                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                                        new LatLng(LKL.getLatitude(),
-                                                LKL.getLongitude()), 10.0f));
-                            }
-                        } else {
-                            mMap.moveCamera(CameraUpdateFactory
-                                    .newLatLngZoom(defaultLocation, 10.0f));
-                        }
-                    }
-                });
-            }
-        } catch (SecurityException e)  {
-            Log.e("Exception: %s", e.getMessage(), e);
-        }
     }
 
     /**
@@ -182,7 +77,6 @@ public class MapsActivity extends FragmentActivity implements
     {
         mMap = googleMap;
 
-        updateLocationUI();
         //getDeviceLocation(); //broken
 
         // add a marker at indianapolis
